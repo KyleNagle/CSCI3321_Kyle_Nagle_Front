ff8a3b31572ddcb5168f9571b71017be0f66cf57
Kyle <kylenagle62gmail.com>
2021-03-30 23:13:53 -0500

    "completed" tic tac toe

--- script/tic-tac-toe.js
+++ script/tic-tac-toe.js
@@ -1,38 +1,42 @@
 var gameOver = false;
 var currentPlayer = 'X';
 var totalMove = 0;
+var tie = false;
 function placeMarker(spotID){
+    
 if(!gameOver){
 var spot = document.getElementById(spotID);
-    //if(spot ==!'X'||spot==!'O'){
-        spot.innerHTML = 'X';
-      //  }
-
-// if(currentPlayerWon()){
-//     updateGamestatus()}
-//   else {
-//    spot.innerHTML = currentPlayer;
-//  totalMove++
-//     if(currentPlayer === 'X'){
-//          currentPlayer = 'O';}
-    //else{
-    // currentPlayer = 'X'    
-    //}
-// else, do nothing
+    if(spot.innerHTML ==='X'||spot.innerHTML === 'O'){
+}
+        else{
+       spot.innerHTML = currentPlayer;
+       totalMove++;
+       
+     if(currentPlayer === 'X'){
+          currentPlayer = 'O';
+          currentPlayerWon();
+          updateGameStatus();  
+        }
+    else{
+     currentPlayer = 'X';  
+     currentPlayerWon();
+     updateGameStatus();  
     }
 }
-
-
+}
+else{
+}
+}
 function currentPlayerWon(){
-    var s1 = document.getElementById('1');
-    var s2 = document.getElementById('2');
-    var s3 = document.getElementById('3');
-    var s4 = document.getElementById('4');
-    var s5 = document.getElementById('5');
-    var s6 = document.getElementById('6');
-    var s7 = document.getElementById('7');
-    var s8 = document.getElementById('8');
-    var s9 = document.getElementById('9');
+    var s1 = document.getElementById('s1');
+    var s2 = document.getElementById('s2');
+    var s3 = document.getElementById('s3');
+    var s4 = document.getElementById('s4');
+    var s5 = document.getElementById('s5');
+    var s6 = document.getElementById('s6');
+    var s7 = document.getElementById('s7');
+    var s8 = document.getElementById('s8');
+    var s9 = document.getElementById('s9');
 
     if (s1.innerHTML === s2.innerHTML && s2.innerHTML === s3.innerHTML || 
         s4.innerHTML === s5.innerHTML && s5.innerHTML === s6.innerHTML ||
@@ -41,29 +45,40 @@ function currentPlayerWon(){
      s2.innerHTML === s5.innerHTML && s5.innerHTML === s8.innerHTML ||
      s3.innerHTML === s6.innerHTML && s6.innerHTML === s9.innerHTML ||
      s1.innerHTML === s5.innerHTML && s5.innerHTML === s9.innerHTML ||
-     s3.innerHTML === s5.innerHTML && s5.innerHTML === s3.innerHTML){
+     s3.innerHTML === s5.innerHTML && s5.innerHTML === s7.innerHTML){
          gameOver=true;
      }
-    
-    // 
-    //else {gameOver = false}
-    // if(!gameOver){
-    // We need to check for tie. 
-    // check the counter : totalMove ===9 then tie  
-    // if it's a tie then gameOver = true
-    //}
+    else{
+        if(totalMove ==='9'){
+            gameOver=false;
+        }
+
+    }
     return gameOver;
 }
 
 
 function updateGameStatus(){
 var statusBoard = document.getElementById('status');
-//if(gameOver){
-// some message  
-//}
-//else{
-//   if not a tie prompt user to make a move
-// else display message   
-//}
+if(gameOver){
+     if(!tie){
+            if(currentPlayer ==='O'){
+                statusBoard.innerHTML = `Player X wins!`;}
+                else{
+                    statusBoard.innerHTML = `Player O wins!`;
+                }
+        }
+        else{
+            statusBoard.innerHTML = `Tie!`;
+        }
 
+    }
+    else{
+        if(totalMove ==='9'){
+            statusBoard.innerHTML = `Tie!`;
+        }
+        else{
+        statusBoard.innerHTML = `Player ${currentPlayer}'s turn ${totalMove}`;
+    }
+    }
 }
